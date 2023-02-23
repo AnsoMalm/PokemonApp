e5167bbd642ada31ee28902988c3b374c1937108
AnsoMalm <112514545+AnsoMalm@users.noreply.github.com>
2023-02-23 17:27:40 +0100

    fixed funktioner with apidata

--- script.js
+++ script.js
@@ -1,69 +1,79 @@
 
-const buttons = {
-	championButton: document.querySelector('#champion-btn'),
-	teamButton: document.querySelector('#team-btn')
-}
 const body = document.querySelector('body')
-const pokemonList = document.querySelector('#pokemon-list-container')
 
+const firstPage = {
+    firstPageText: document.querySelector('#upper-text'),
+    championButton: document.querySelector('#champion-btn'),
+    teamButton: document.querySelector('#team-btn')
+}
+const viewsOne = {
+    pageOne: document.querySelector('.vy-1'),
+    inputPokemon: document.querySelector('.search-pokemon'),
+}
+const viewsTwo = {
+    pageTwo: document.querySelector('.vy-2-invisible'),
+    myTeam: document.querySelector('.my-team')
+}
 
-buttons.championButton.addEventListener('click', () => { 
-	//console.log('button clicked')
-	const overlay = document.createElement('div')
-	const content = document.createElement('section')
+firstPage.championButton.addEventListener('click', () => {
+    viewsOne.pageOne.style.display = 'block'
+    viewsTwo.pageTwo.style.display = 'none'
+})
 
-	overlay.classList.add('overlay')
-	content.innerText = 'Pokemon-list'
-
-	overlay.append(content)
-
-	overlay.addEventListener('click', () => {
-		console.log('Clicked overlay')
-		overlay.remove()
-	})
-
-	content.addEventListener('click', event => {
-		console.log('Clicked content. Overlay should NOT close')
-		event.stopPropagation()
-	})
-
-	body.append(overlay)
+firstPage.teamButton.addEventListener('click', () => {
+    viewsOne.pageOne.style.display = 'none'
+    viewsTwo.pageTwo.style.display = 'block'
 
 })
 
+/*viewsOne.inputPokemon.addEventListener('keydown', async () => {
+    console.log(event.key)
 
-/*
-function addPokemonlist() {
-	//const newDiv = document.createElement('div');
-	const label = document.createElement('label')
-	label.innerText = 'Sök efter din Pokemon'
-	const inputField = document.createElement('input')
-	inputField.innerText = 'Sök i raden här...' 
+    const searchString = viewsOne.input.value
+    const url .......    + searchString
 
-//newDiv.append(pokemonList)
-label.append(pokemonList)
-inputField.append(pokemonList)
+
+})*/
+
+//Hämta data med namn på pokemon. 
+let nameUrl = 'https://pokeapi.co/api/v2/pokemon?limit=10&offset=0'
+
+async function GetAPI(nameUrl) {
+    const response = await fetch(nameUrl)
+    const data = await response.json()
+    let dataResult = data.results;
+    return dataResult;
 }
-addPokemonlist()
-/*
-function addMyTeam() {
-	const container2 = document.createElement('div');
 
-}
+async function renderUI() {
+    let pokemons = await GetAPI(nameUrl);
+    let pokemonsContainerEl = document.querySelector('.pokemons-container');
+   
+    pokemons.forEach(async pokemon => {
+        let response = await fetch(pokemon.url);
+        let data = await response.json();
+        let imgUrl = data.sprites.front_default;
 
-//my-team-container
+        pokemon.url = imgUrl;
+        let pokemonCard = document.createElement('article');
+        pokemonCard.innerHTML = `
+			<p>${pokemon.name}</p>
+			<img src="${pokemon.url}">
+		`;
+        pokemonsContainerEl.appendChild(pokemonCard);
+    });
+};
 
-/*	<section id="Pokemon-list-container">
-			<article id="Pokemon-list">
-				<label>Sök efter din Pokemon:</label>
-				<input id="pokemon-sök" type="text" placeholder="Sök i raden här...">
-				<div id="pokemon-container">
-					<div id="pokemon-info">
-					<button id="pic-me">Välj mig</button>
-					<button id="pic-me">Välj mig</button>
-					<button id="pic-me">Välj mig</button>
-					</div>
-				</div>
-			</article>
-		</section>*/
+renderUI();
 
+
+
+// GetAPI(nameUrl)
+// .then(pokemons => {
+// 	pokemonNames.push(...pokemons)
+// }) 
+// .catch(error => {
+//     console.error(error)
+// })
+
+// let pokemonNames = []
