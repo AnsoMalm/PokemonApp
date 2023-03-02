e348ee19496595dd8abb4a306a38f59411a3e415
AnsoMalm <112514545+AnsoMalm@users.noreply.github.com>
2023-03-02 09:35:26 +0100

    change how many letters the user have to enter in my findinputfiled.

--- script.js
+++ script.js
@@ -78,53 +78,58 @@ renderUI();
 
 //funktionen som körs när använder skriver i sökfältet
 findPokemonInput.addEventListener('keyup', async() => {
-    const searchString = findPokemonInput.value; 
-    const matchingPokemon = pokemonList.filter(pokemon => pokemon.name.includes(searchString))
+    const searchString = findPokemonInput.value;
+    if (findPokemonInput.value.length > 2) {
+        const matchingPokemon = pokemonList.filter(pokemon => pokemon.name.includes(searchString))
 
-    //rensar container innan ny pokemon läggs till. 
-    pokemonContainer.innerHTML = "",
+        //rensar container innan ny pokemon läggs till. 
+        pokemonContainer.innerHTML = "",
+    
+        //loopa genom varje matchande pokemon och skapa ett kort för varje 
+        matchingPokemon.forEach(pokemon => {
+            let pokemonCard = document.createElement('article');
+            
+            let pokemonName = document.createElement('p');
+            pokemonName.innerHTML = `${pokemon.name}`;
+    
+            let pokemonImg = document.createElement('div');
+            pokemonImg.innerHTML = `<img src="${pokemon.image}">`;
+    
+            let pokemonButton = document.createElement('button');
+            pokemonButton.innerText = 'Lägg till pokemon';
+    
+            pokemonCard.classList ='pokemon-card';
+            pokemonName.classList = 'pokemon-name';
+            pokemonImg.classList = 'pokemon-img';
+            pokemonButton.classList ='pokemon-button';
+            
+            pokemonCard.append(pokemonName, pokemonImg, pokemonButton);
+            pokemonContainer.append(pokemonCard);
+    
+            //lägg till eventlistner för att lägga till pokemon i MypokemonTeam samt lägga till text som meddelande till användaren
+            pokemonButton.addEventListener('click', () => {
+                  console.log(pokemonButton)
+                if (myPokemonTeam.length < 3) {
+                    addMessageToMyTeam(pokemonButton);
+                    myPokemonTeam.push({ ...pokemon });
+    
+                } else {
+                    addedMessageToMyReserve(pokemonButton);
+                    reserveList.push(pokemon);
+                    
+                }
+                renderMyTeam();
+                renderReserveList();
+                showPokemonTeamStatus();
 
-    //loopa genom varje matchande pokemon och skapa ett kort för varje 
-    matchingPokemon.forEach(pokemon => {
-        let pokemonCard = document.createElement('article');
-        
-        let pokemonName = document.createElement('p');
-        pokemonName.innerHTML = `${pokemon.name}`;
-
-        let pokemonImg = document.createElement('div');
-        pokemonImg.innerHTML = `<img src="${pokemon.image}">`;
-
-        let pokemonButton = document.createElement('button');
-        pokemonButton.innerText = 'Lägg till pokemon';
-
-        pokemonCard.classList ='pokemon-card';
-        pokemonName.classList = 'pokemon-name';
-        pokemonImg.classList = 'pokemon-img';
-        pokemonButton.classList ='pokemon-button';
-        
-        pokemonCard.append(pokemonName, pokemonImg, pokemonButton);
-        pokemonContainer.append(pokemonCard);
-
-        //lägg till eventlistner för att lägga till pokemon i MypokemonTeam samt lägga till text som meddelande till användaren
-        pokemonButton.addEventListener('click', () => {
-              console.log(pokemonButton)
-            if (myPokemonTeam.length < 3) {
-                addMessageToMyTeam(pokemonButton);
-                myPokemonTeam.push({ ...pokemon });
-
-            } else {
-                addedMessageToMyReserve(pokemonButton);
-                reserveList.push(pokemon);
-                
-            }
-            renderMyTeam();
-            renderReserveList();
-            showPokemonTeamStatus();
-
+            })
+   
         })
+    };
+});
 
-    });
-})
+   
+
 
 //Visar lagets status i myPokemonTeam, ifall du kan fylla på mer i laget eller inte
 
